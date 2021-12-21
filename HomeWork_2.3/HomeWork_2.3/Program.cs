ad762d4a3ee060d60bd3a756b1c2539bd4c45b74
Tanya <yallina.ta@yandex.ru>
2021-12-22 08:16:40 +1000

    Finished
    Finished

---
+++ HomeWork_2.3/HomeWork_2.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.WriteLine("Введите число: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if(number % 2 == 0)
+            {
+                Console.WriteLine("Число - четное!");
+            }
+            else
+            {
+                Console.WriteLine("Число - нечетное!");
+            }
+            Console.ReadKey();
+            return;
+
+        }
+    }
+}
