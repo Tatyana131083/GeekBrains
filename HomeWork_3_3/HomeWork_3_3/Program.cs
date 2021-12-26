b01234a166aa6a142ab3211eea17c1c369d52d04
Tanya <yallina.ta@yandex.ru>
2021-12-27 08:00:44 +1000

    Finished
    Finished

---
+++ HomeWork_3_3/HomeWork_3_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeWork_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string greeting = Console.ReadLine();
+            for (int i = greeting.Length-1; i >= 0; i--)
+            {
+                Console.Write(greeting[i]); // построчный вывод каждого символа строки
+            }
+            Console.WriteLine("");
+        }
+
+    }
+}
